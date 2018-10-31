d0ad525dfaa6ad1b436e4f31364d170dfb1e53b6
Paul Gilfedder <>
2018-10-31 11:16:35 +0000

    Fully working graphql application

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://lyrical:lyr1cal@ds141813.mlab.com:41813/pglyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
